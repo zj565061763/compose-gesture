8c0ff9e141233d0a745c6d3cde54aff6c76c65cd
565061763 <565061763@qq.com>
2022-08-02 00:06:05 +0800

    demo

--- app/src/main/java/com/sd/demo/comopse_gesture/MainActivity.kt
+++ app/src/main/java/com/sd/demo/comopse_gesture/MainActivity.kt
@@ -29,7 +29,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    SampleVelocityTracker()
+                    SampleOnPointerChange()
                 }
             }
         }
