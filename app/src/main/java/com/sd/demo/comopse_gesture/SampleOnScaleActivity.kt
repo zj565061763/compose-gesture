690727a5ad00caa09928edd29fd5a28f05559706
565061763 <565061763@qq.com>
2022-08-02 15:43:24 +0800

    fOnScale

--- app/src/main/java/com/sd/demo/comopse_gesture/SampleOnScaleActivity.kt
+++ app/src/main/java/com/sd/demo/comopse_gesture/SampleOnScaleActivity.kt
@@ -3,12 +3,18 @@ package com.sd.demo.comopse_gesture
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import com.sd.demo.comopse_gesture.ui.theme.ComopsegestureTheme
 import com.sd.lib.compose.gesture.fOnScale
 
@@ -32,11 +38,23 @@ class SampleOnScaleActivity : ComponentActivity() {
 private fun SampleOnScale(
     modifier: Modifier = Modifier,
 ) {
+    var scale by remember { mutableStateOf(1f) }
     Box(
+        contentAlignment = Alignment.Center,
         modifier = modifier
             .fillMaxSize()
             .fOnScale { centroid, zoomChange ->
                 logMsg { "scale centroid:$centroid zoomChange:$zoomChange" }
+                scale *= zoomChange
             }
-    )
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.scale),
+            contentDescription = "",
+            contentScale = ContentScale.FillWidth,
+            modifier = Modifier
+                .fillMaxWidth()
+                .scale(scale)
+        )
+    }
 }
