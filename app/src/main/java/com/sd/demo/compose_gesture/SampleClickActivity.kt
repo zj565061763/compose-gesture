21456c7e5bf85631092dacebca5ebee78be59e29
zj565061763 <565061763@qq.com>
2023-04-14 18:16:41 +0800

    format

--- app/src/main/java/com/sd/demo/compose_gesture/SampleClickActivity.kt
+++ app/src/main/java/com/sd/demo/compose_gesture/SampleClickActivity.kt
@@ -5,7 +5,6 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.sd.demo.compose_gesture.ui.theme.AppTheme
@@ -16,9 +15,7 @@ class SampleClickActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             AppTheme {
-                Surface(modifier = Modifier.fillMaxSize()) {
-                    SampleClick()
-                }
+                SampleClick()
             }
         }
     }
--- lib/src/main/java/com/sd/lib/compose/gesture/FClick.kt
+++ lib/src/main/java/com/sd/lib/compose/gesture/FClick.kt
@@ -7,15 +7,15 @@ import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.input.pointer.pointerInput
 
 fun Modifier.fClick(
-    onPress: (suspend PressGestureScope.(Offset) -> Unit)? = null,
-    onTap: ((Offset) -> Unit)? = null,
     onDoubleTap: ((Offset) -> Unit)? = null,
     onLongPress: ((Offset) -> Unit)? = null,
+    onPress: (suspend PressGestureScope.(Offset) -> Unit)? = null,
+    onTap: ((Offset) -> Unit)? = null,
 ) = pointerInput(Unit) {
     detectTapGestures(
-        onPress = onPress ?: {},
-        onTap = onTap,
         onDoubleTap = onDoubleTap,
         onLongPress = onLongPress,
+        onPress = onPress ?: {},
+        onTap = onTap,
     )
 }
