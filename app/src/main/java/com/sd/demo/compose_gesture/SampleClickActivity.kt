4841d9215095e6bb448869031987880d20b317fa
zj565061763 <565061763@qq.com>
2023-09-08 11:22:31 +0800

    demo

--- app/src/main/java/com/sd/demo/compose_gesture/SampleClickActivity.kt
+++ app/src/main/java/com/sd/demo/compose_gesture/SampleClickActivity.kt
@@ -3,11 +3,16 @@ package com.sd.demo.compose_gesture
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import com.sd.demo.compose_gesture.ui.theme.AppTheme
+import com.sd.lib.compose.gesture.fClick
 import com.sd.lib.compose.gesture.fCombinedClick
 
 class SampleClickActivity : ComponentActivity() {
@@ -25,19 +30,34 @@ class SampleClickActivity : ComponentActivity() {
 private fun Sample(
     modifier: Modifier = Modifier,
 ) {
-    Box(
-        modifier = modifier
-            .fillMaxSize()
-            .fCombinedClick(
-                onClick = {
-                    logMsg { "onClick" }
-                },
-                onDoubleClick = {
-                    logMsg { "onDoubleClick" }
-                },
-                onLongClick = {
-                    logMsg { "onLongClick" }
-                },
-            )
-    )
+    Column(
+        modifier = modifier.fillMaxSize(),
+    ) {
+        Box(
+            modifier = modifier
+                .fillMaxWidth()
+                .weight(1f)
+                .background(Color.LightGray)
+                .fClick {
+                    logMsg { "fClick" }
+                }
+        )
+        Box(
+            modifier = modifier
+                .fillMaxWidth()
+                .weight(1f)
+                .background(Color.DarkGray)
+                .fCombinedClick(
+                    onClick = {
+                        logMsg { "onClick" }
+                    },
+                    onDoubleClick = {
+                        logMsg { "onDoubleClick" }
+                    },
+                    onLongClick = {
+                        logMsg { "onLongClick" }
+                    },
+                )
+        )
+    }
 }
