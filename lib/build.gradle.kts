f36f24059212b24a6814c5dccd24f2deae8d8d21
zjun <565061763@qq.com>
2024-01-12 12:47:19 +0800

    1.3.3

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 val libGroupId = "com.sd.lib.android"
 val libArtifactId = "compose-gesture"
-val libVersionName = "1.3.2"
+val libVersionName = "1.3.3"
 
 android {
     namespace = "com.sd.lib.compose.gesture"
