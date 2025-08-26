6f636c7da7b70282bf2e7ad24bac1ff68a75d8f7
zjun <565061763@qq.com>
2025-08-26 14:27:11 +0800

    comment

--- lib/src/main/java/com/sd/lib/compose/gesture/Utils.kt
+++ lib/src/main/java/com/sd/lib/compose/gesture/Utils.kt
@@ -1,10 +1,10 @@
 package com.sd.lib.compose.gesture
 
-import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.input.pointer.AwaitPointerEventScope
 import androidx.compose.ui.input.pointer.PointerEvent
 import androidx.compose.ui.input.pointer.PointerEventPass
 import androidx.compose.ui.input.pointer.PointerInputChange
+import androidx.compose.ui.input.pointer.positionChangedIgnoreConsumed
 import androidx.compose.ui.util.fastAny
 
 /** 等待所有指针都抬起 */
@@ -23,10 +23,12 @@ fun PointerEvent.fHasPointerPressed(): Boolean {
   return changes.fastAny { it.pressed }
 }
 
+/** 是否有被消费的移动事件 */
 fun PointerEvent.fHasConsumedPositionChange(): Boolean {
   return changes.fastAny { it.fIsConsumedPositionChange() }
 }
 
+/** 是否被消费的移动事件 */
 fun PointerInputChange.fIsConsumedPositionChange(): Boolean {
-  return isConsumed && ((position - previousPosition) != Offset.Zero)
+  return isConsumed && positionChangedIgnoreConsumed()
 }
